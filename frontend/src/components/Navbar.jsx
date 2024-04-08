da9ab3aace815f31935c8bbc07665429788bd8aa
Lev Kadatskyi <levkadatscky@gmail.com>
2024-04-08 14:46:12 +0300

    added dynamic change of navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -14,11 +14,62 @@ import HomeIcon from "@mui/icons-material/Home";
 import { Link, useLocation } from "react-router-dom";
 import InfoIcon from "@mui/icons-material/Info";
 import BorderColorIcon from "@mui/icons-material/BorderColor";
+import { IconButton } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export default function Navbar(props) {
   const { drawerWidth, content } = props;
   const location = useLocation();
   const path = location.pathname;
+  const [open, setOpen] = React.useState(false);
+
+  const changeOpenStatus = () => {
+    setOpen(!open);
+  };
+
+  const myDrawer = (
+    <div>
+      <Toolbar />
+      <Box sx={{ overflow: "auto" }}>
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton component={Link} to="/" selected={"/" === path}>
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText primary={"Home"} />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem disablePadding>
+            <ListItemButton
+              component={Link}
+              to="/about"
+              selected={"/about" === path}
+            >
+              <ListItemIcon>
+                <InfoIcon />
+              </ListItemIcon>
+              <ListItemText primary={"About"} />
+            </ListItemButton>
+          </ListItem>
+
+          <ListItem disablePadding>
+            <ListItemButton
+              component={Link}
+              to="/create"
+              selected={"/create" === path}
+            >
+              <ListItemIcon>
+                <BorderColorIcon />
+              </ListItemIcon>
+              <ListItemText primary={"Create"} />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Box>
+    </div>
+  );
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
@@ -27,6 +78,13 @@ export default function Navbar(props) {
         sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}
       >
         <Toolbar>
+          <IconButton
+            onClick={changeOpenStatus}
+            color="inherit"
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
           <Typography variant="h6" noWrap component="div">
             Our App
           </Typography>
@@ -35,6 +93,7 @@ export default function Navbar(props) {
       <Drawer
         variant="permanent"
         sx={{
+          display: { xs: "none", sm: "block" },
           width: drawerWidth,
           flexShrink: 0,
           [`& .MuiDrawer-paper`]: {
@@ -43,49 +102,28 @@ export default function Navbar(props) {
           },
         }}
       >
-        <Toolbar />
-        <Box sx={{ overflow: "auto" }}>
-          <List>
-            <ListItem disablePadding>
-              <ListItemButton component={Link} to="/" selected={"/" === path}>
-                <ListItemIcon>
-                  <HomeIcon />
-                </ListItemIcon>
-                <ListItemText primary={"Home"} />
-              </ListItemButton>
-            </ListItem>
+        {myDrawer}
+      </Drawer>
 
-            <ListItem disablePadding>
-              <ListItemButton
-                component={Link}
-                to="/about"
-                selected={"/about" === path}
-              >
-                <ListItemIcon>
-                  <InfoIcon />
-                </ListItemIcon>
-                <ListItemText primary={"About"} />
-              </ListItemButton>
-            </ListItem>
-
-            <ListItem disablePadding>
-              <ListItemButton
-                component={Link}
-                to="/create"
-                selected={"/create" === path}
-              >
-                <ListItemIcon>
-                  <BorderColorIcon />
-                </ListItemIcon>
-                <ListItemText primary={"Create"} />
-              </ListItemButton>
-            </ListItem>
-          </List>
-        </Box>
+      <Drawer
+        variant="temporary"
+        open={open}
+        onClose={changeOpenStatus}
+        sx={{
+          display: { xs: "block", sm: "none" },
+          width: drawerWidth,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+      >
+        {myDrawer}
       </Drawer>
       <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
         <Toolbar />
-            {content}
+        {content}
       </Box>
     </Box>
   );
