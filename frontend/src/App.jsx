ed4970491c1a0917be18359aa76b5070d5bb4814
Lev Kadatskyi <levkadatscky@gmail.com>
2024-04-08 14:24:54 +0300

    added navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,21 +1,27 @@
-import './App.css'
-import {Routes, Route} from 'react-router-dom'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
 
-import Home from './components/Home'
-import About from './components/About'
-import Create from './components/Create'
+import Home from "./components/Home";
+import About from "./components/About";
+import Create from "./components/Create";
+import Navbar from "./components/Navbar";
 
 function App() {
-  return(
-    <div className='App'>
-      <Routes>
-        <Route path='' element={<Home/>} />
-        <Route path='/about' element={<About/>} />
-        <Route path='/create' element={<Create/>} />
-      </Routes>
-
+  const width = 220;
+  return (
+    <div className="App">
+      <Navbar
+        drawerWidth={width}
+        content={
+          <Routes>
+            <Route path="" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/create" element={<Create />} />
+          </Routes>
+        }
+      />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
