648d73d8f0724f5d33b2bdb830d1d70ce993c574
Lev Kadatskyi <levkadatscky@gmail.com>
2024-04-12 12:12:55 +0300

    added linter and formatter

--- frontend/.eslintrc.cjs
+++ frontend/.eslintrc.cjs
@@ -6,6 +6,7 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react/jsx-runtime',
     'plugin:react-hooks/recommended',
+    'plugin:prettier/recommended',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
@@ -13,9 +14,7 @@ module.exports = {
   plugins: ['react-refresh'],
   rules: {
     'react/jsx-no-target-blank': 'off',
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'prettier/prettier': ['warn', { endOfLine: 'auto', tabWidth: 2 }],
   },
-}
+};
